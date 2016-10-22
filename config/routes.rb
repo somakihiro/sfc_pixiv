64282fc1d801e38ed30149b6f5ad7f3105d33e7c
chikuwa111 <take0shun1111@gmail.com>
2016-10-23 04:01:19 +0900

    とりあえず絵本は作れるように

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'new' => 'searches#new'
   post 'get' => 'searches#get'
   get 'image' => 'searches#get_image'
-  get 'keitaiso' => 'searches#get_keitaiso'
+  post 'keitaiso' => 'searches#get_keitaiso'
   resources :picture_stories
 end

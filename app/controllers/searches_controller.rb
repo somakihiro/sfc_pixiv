df37a0656b73764394bcb8ef665eae7c43b17df3
chikuwa111 <take0shun1111@gmail.com>
2016-10-22 16:44:17 +0900

    一旦コミット

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -29,4 +29,19 @@ class SearchesController < ApplicationController
     @response = json['response']
   end
 
+  def get_image
+    # url_part = url.slice!(/http:\/\/i1\.pixiv\.net\//)
+    @img_url = 'http://i1.pixiv.net/c/150x150/img-master/img/2016/09/01/20/11/43/58763560_p0_master1200.jpg'
+    uri = URI.parse(@img_url)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["Referer"] = 'https://public-api.secure.pixiv.net' # httpリクエストヘッダの追加
+
+    @res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.request(req)
+    end
+
+    File.open("public/images/image.jpeg", "wb") {|f| f.write(@res.read)}
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'new' => 'searches#new'
   post 'get' => 'searches#get'
+  get 'image' => 'searches#get_image'
 end
